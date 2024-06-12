348082cca494ea4ee8daf08fdedfb5e04f00fa93
duc-lap <rumbonaruson@gmail.com>
2024-06-13 05:01:15 +0700

    Add: Spot light (but not yet)

--- js/main.js
+++ js/main.js
@@ -20,66 +20,179 @@ camera.position.z = 5; // move cam back 5 units
 const renderer = new THREE.WebGLRenderer({antialias: true}); // smooth edges
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xffffff, 1); // background color
+// Optimize the lights and shadows
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild(renderer.domElement);
+
+
+
+function createPainting(imagePath, width, height, position) {
+    const textureLoader = new THREE.TextureLoader();
+    const paintingTexture = textureLoader.load(imagePath);
+    const paintingGeometry = new THREE.BoxGeometry(width, height);
+    const paintingMaterial = new THREE.MeshBasicMaterial({ map: paintingTexture });
+    const painting = new THREE.Mesh(paintingGeometry, paintingMaterial);
+    painting.position.copy(position);
+    return painting;
+}
+
+const paintingHeight = 8;
+// on front wall
+const painting01 = createPainting(
+    'public/artworks/0.jpg',
+    20,
+    10,
+    new THREE.Vector3(15, paintingHeight, -60)
+)
+const painting02 = createPainting(
+    'public/artworks/1.jpg',
+    20,
+    10,
+    new THREE.Vector3(-15, paintingHeight, -60)
+)
+scene.add(painting01, painting02)
+// on left wall
+const painting03 = createPainting(
+    'public/artworks/2.jpg',
+    20,
+    10,
+    new THREE.Vector3(-40, paintingHeight, -25)
+)
+painting03.rotateY(Math.PI / 2);
+const painting04 = createPainting(
+    'public/artworks/3.jpg',
+    20,
+    10,
+    new THREE.Vector3(-40, paintingHeight, 30)
+)
+painting04.rotateY(Math.PI / 2);
+scene.add(painting03, painting04);
+// on right wall
+const painting05 = createPainting(
+    'public/artworks/4.jpg',
+    20,
+    10,
+    new THREE.Vector3(40, paintingHeight, -25)
+)
+painting05.rotateY(-Math.PI / 2);
+const painting06 = createPainting(
+    'public/artworks/5.jpg',
+    20,
+    10,
+    new THREE.Vector3(40, paintingHeight, 30)
+)
+painting06.rotateY(-Math.PI / 2);
+scene.add(painting05, painting06);
+
+// Painting info
+
 
 // Light
     // AmbientLight 
-const ambientLight = new THREE.AmbientLight(0x101010, 1.0) // color, intensity, distance. decay
-// light folow camera
-ambientLight.position.copy(camera.position);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6) // color, intensity, distance. decay
 scene.add(ambientLight);
 
-    // Directional Light
-const sunLight = new THREE.DirectionalLight(0xdddddd, 1.0); // color, intensity
-sunLight.position.y = 15;
-scene.add(sunLight);
+//     // Directional Light
+// const sunLight = new THREE.DirectionalLight(0xdddddd, 1.0); // color, intensity
+// sunLight.position.y = 15;
+// scene.add(sunLight);
+
+// const spotlight = new THREE.Dir
+
+// Spotlights can be used to simulate ceiling-mounted lights or track lights that focus on specific areas or artworks.
+function createSpotlight(x, y, z, intensity, targetPosition) {
+    const spotlight = new THREE.SpotLight(0xffffff, intensity);
+    spotlight.position.set(x, y, z);
+    spotlight.target.position.copy(targetPosition);
+    spotlight.castShadow = true;
+    spotlight.angle = Math.PI / 6; // 30 degrees
+    spotlight.penumbra = 0.9;
+    spotlight.decay = 2;
+    spotlight.distance = 40;
+    spotlight.shadow.mapSize.width = 1024;
+    spotlight.shadow.mapSize.height = 1024;
+    scene.add(spotlight);
+    scene.add(spotlight.target);
+
+    const spotLightHelper = new THREE.SpotLightHelper(spotlight);
+    scene.add(spotLightHelper);
+
+    // return spotlight;
+  }
+// Add spotlights to the scene
+createSpotlight(15, 25, -50, 1.5, painting01.position);
+createSpotlight(-15, 25, -50, 1.5, painting02.position);
+createSpotlight(-30, 25, -25, 1.5, painting03.position);
+createSpotlight(-30, 25, 30, 1.5, painting04.position);
+createSpotlight(30, 25, -25, 1.5, painting05.position);
+createSpotlight(30, 25, 30, 1.5, painting06.position);
 
 // create room space
 // load floor texture
-// const textureLoader = new THREE.TextureLoader().load('./img/Floor.jpg');
 const textureLoader = new THREE.TextureLoader();
+
+// create the floor plane
 const floorTexture = textureLoader.load("public/img/floor.jpg");
 floorTexture.wrapS = THREE.RepeatWrapping; // wrapS is horizonl direction
 floorTexture.wrapT = THREE.RepeatWrapping; // wrapT verical direction
 floorTexture.repeat.set(1, 5); // texture repeat time
-
-// create the floor plane
 let planeGeometry = new THREE.PlaneGeometry(80, 120);
-let planeMaterial = new THREE.MeshBasicMaterial( {
-    //color: 0xffff00,
+let planeMaterial = new THREE.MeshPhongMaterial( {
     map: floorTexture,
     side: THREE.DoubleSide // render both side of plane
 } );
 let plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.rotateX(Math.PI / 2); // pi/2 radian = 90 degrees
-// plane.position.y = -Math.PI;
-
 scene.add(plane);
+
+// create ceilling
+const ceilingTexture = textureLoader.load("public/img/ceilling.jpg");
+ceilingTexture.wrapS = THREE.RepeatWrapping;
+ceilingTexture.wrapT = THREE.RepeatWrapping;
+ceilingTexture.repeat.set(1, 10);
+const ceilingGeometry = new THREE.PlaneGeometry(80, 120);
+const ceilingMaterial = new THREE.MeshLambertMaterial({
+    map: ceilingTexture,
+    side: THREE.DoubleSide
+});
+const ceiling = new THREE.Mesh(ceilingGeometry, ceilingMaterial);
+ceiling.rotateX(Math.PI / 2);
+ceiling.position.y = 30-5;
+scene.add(ceiling);
 
 // create the walls
 const wallGroup = new THREE.Group(); // create a group to hold the walls
 wallGroup.position.y = 10;
 scene.add(wallGroup);
 
+// add wall texture
+const wallTexture = textureLoader.load('public/img/white-brick-wall.jpg')
+wallTexture.wrapS = THREE.RepeatWrapping;
+wallTexture.wrapT = THREE.RepeatWrapping;
+wallTexture.repeat.set(1, 1);
+
+const wallMeterial = new THREE.MeshLambertMaterial({ map: wallTexture });
+
 const wallHeight = 30;
 // front wall
 const frontWall = new THREE.Mesh(
     new THREE.BoxGeometry(80, wallHeight, 0.001),
-    new THREE.MeshBasicMaterial({ color: 'green' })
+    wallMeterial
 );
 frontWall.translateZ(-60);
 
 // back wall
 const backWall = new THREE.Mesh(
     new THREE.BoxGeometry(80, wallHeight, 0.001),
-    new THREE.MeshBasicMaterial({ color: 'cyan' })
+    wallMeterial
 );
 backWall.translateZ(60);
 
 // left wall
 const leftWall = new THREE.Mesh(
     new THREE.BoxGeometry(120, wallHeight, 0.001),
-    new THREE.MeshBasicMaterial({ color: 'yellow' })
+    wallMeterial
 );
 leftWall.rotateY(Math.PI / 2);
 leftWall.position.x = -40;
@@ -87,7 +200,7 @@ leftWall.position.x = -40;
 // right wall
 const rightWall = new THREE.Mesh(
     new THREE.BoxGeometry(120, wallHeight, 0.001),
-    new THREE.MeshBasicMaterial({ color: 'blue' })
+    wallMeterial
 );
 rightWall.rotateY(Math.PI / 2);
 rightWall.position.x = 40;
@@ -95,11 +208,9 @@ rightWall.position.x = 40;
 wallGroup.add(frontWall, backWall, leftWall, rightWall);
 
 // loop through each wall, create bounding box (for collision) and add texture
-const wallTexture = textureLoader.load('public/img/white-brick-wall.jpg')
 for (let i = 0; i < wallGroup.children.length; i++) {
     wallGroup.children[i].BBox = new THREE.Box3();
     wallGroup.children[i].BBox.setFromObject(wallGroup.children[i]); // add bbox for walls
-    wallGroup.children[i].material.map = wallTexture; // at texture for walls
 }
 // check if player intersects with the wall
 function checkCollision() {
@@ -122,41 +233,30 @@ function checkCollision() {
 }
 
 
-// create ceilling
-const ceilingGeometry = new THREE.PlaneGeometry(80, 120);
-const ceilingMaterial = new THREE.MeshBasicMaterial({
-    color: 'purple',
-    side: THREE.DoubleSide
-});
-const ceiling = new THREE.Mesh(ceilingGeometry, ceilingMaterial);
-ceiling.rotateX(Math.PI / 2);
-ceiling.position.y = wallHeight-5;
-scene.add(ceiling);
+// Enable shadows on objects
+plane.receiveShadow = true;
+ceiling.receiveShadow = true;
+frontWall.castShadow = true;
+frontWall.receiveShadow = true;
+leftWall.castShadow = true;
+leftWall.receiveShadow = true;
+rightWall.castShadow = true;
+rightWall.receiveShadow = true;
+backWall.castShadow = true;
+backWall.receiveShadow = true;
+painting01.castShadow = true;
+painting01.receiveShadow = true;
+painting02.castShadow = true;
+painting02.receiveShadow = true;
+painting03.castShadow = true;
+painting03.receiveShadow = true;
+painting04.castShadow = true;
+painting04.receiveShadow = true;
+painting05.castShadow = true;
+painting05.receiveShadow = true;
+painting06.castShadow = true;
+painting06.receiveShadow = true;
 
-function createPainting(imagePath, width, height, position) {
-    const textureLoader = new THREE.TextureLoader();
-    const paintingTexture = textureLoader.load(imagePath);
-    const paintingGeometry = new THREE.BoxGeometry(width, height);
-    const paintingMaterial = new THREE.MeshBasicMaterial({ map: paintingTexture });
-    const painting = new THREE.Mesh(paintingGeometry, paintingMaterial);
-    painting.position.copy(position);
-    return painting;
-}
-
-const paintingHeight = 8;
-const painting01 = createPainting(
-    'public/artworks/0.jpg',
-    10,
-    5,
-    new THREE.Vector3(10, paintingHeight, -60)
-)
-const painting02 = createPainting(
-    'public/artworks/1.jpg',
-    10,
-    5,
-    new THREE.Vector3(-10, paintingHeight, -60)
-)
-scene.add(painting01, painting02);
 
 // Controls
 const controls = new PointerLockControls(camera, document.body);
@@ -248,3 +348,10 @@ let renderLoop = function() {
 }
 
 renderLoop();
+
+// Adjust scene on window resize
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}, false);
